fb3f1fba927f064db5c97fd76e8736485aa24c90
baron070981 <baron070981@gmail.com>
2025-09-16 16:20:46 +0300

    --

--- DRFTaskScheduler/TaskSheduler/TaskSheduler/settings.py
+++ DRFTaskScheduler/TaskSheduler/TaskSheduler/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from . import secret
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-av2&5sb96=+mx9%*^8m$yg+-q+)6aq6+(rk%8)q97$0s$uc6#a'
+SECRET_KEY = secret.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
